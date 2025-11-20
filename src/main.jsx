3ff8a77896a3bcae981ff3c7328bf19cee1c7793
dowevip <42400509+dowevip@users.noreply.github.com>
2025-11-20 13:41:46 +0800

    Add main.jsx to render ShenAoPortfolio component

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ShenAoPortfolio from './ShenAoPortfolio'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ShenAoPortfolio />
+  </React.StrictMode>,
+)
